707b277e4f94579e97d1626862ad68258adefce5
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-01-21 11:17:06 +0700

    nuxt layouts

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NuxtLink to="/">Home</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/about">About</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/products">Products</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
         <h2>About Pages</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
             voluptatem eveniet sit. Reiciendis!</p>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,20 +1,5 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NuxtLink to="/">Home</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/about">About</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink to="/products">Products</NuxtLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
         <h2>Product Pages</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
             voluptatem eveniet sit. Reiciendis!</p>
@@ -24,7 +9,9 @@
 </template>
 
 <script setup>
-
+definePageMeta({
+    layout: 'products'
+})
 </script>
 
 <style scoped>
