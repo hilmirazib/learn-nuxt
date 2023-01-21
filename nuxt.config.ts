a183032ce7c275b1b6fac83aa33a643353c72d03
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-01-21 14:59:37 +0700

    finish

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,17 @@ export default defineNuxtConfig({
         '@nuxtjs/tailwindcss'
     ],
     app: {
-    head: {
-      title: 'Nuxt Dojo',
-      meta: [
-        {name: 'description', content: 'Everything about Nuxt 3'}
-      ],
-      link: [
-        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
-      ]
+        head: {
+            title: 'Nuxt Dojo',
+            meta: [
+                { name: 'description', content: 'Everything about Nuxt 3' }
+            ],
+            link: [
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+            ]
+        }
+    },
+    runtimeConfig: {
+        currencyKey: process.env.CURRENCY_API_KEY
     }
-  }
 })
