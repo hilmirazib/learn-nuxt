4a94dbce7c48f444d6c8c0f7b949f1d778d4ab24
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-01-21 10:51:31 +0700

    nuxt link

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        <p>About</p>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <h2>About Pages</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
+            voluptatem eveniet sit. Reiciendis!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
+            voluptatem eveniet sit. Reiciendis!</p>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        <p>Index</p>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <h2>Home Pages</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
+            voluptatem eveniet sit. Reiciendis!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta fugiat modi blanditiis magnam hic tempore aut
+            voluptatem eveniet sit. Reiciendis!</p>
     </div>
 </template>
 
