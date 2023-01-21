5ac90e7df9ed679089c09c46e9a0ec63a6f6c0b8
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2023-01-21 10:43:11 +0700

    install nuxt 3, create pages, route

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Route {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore distinctio optio quisquam.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Product Page</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
